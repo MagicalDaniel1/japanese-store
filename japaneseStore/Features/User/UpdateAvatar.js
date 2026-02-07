02f8bfee6b7e26529442704c93e1126ad88730c7
drajan <anastasia.fed2009@gmail.com>
2026-02-07 19:10:08 +0200

    Add files via upload

---
+++ japaneseStore/Features/User/UpdateAvatar.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const BASE = "http://localhost:3000/api/user";
+
+async function updateAvatar(name, avatarUrl) {
+    try {
+        const res = await axios.patch(`${BASE}/${name}`, { avatar: avatarUrl });
+        return res.data;
+    } catch (err) {
+        console.error("Ошибка updateAvatar:", err.response?.data || err.message);
+        throw err;
+    }
+}
+
+module.exports = { updateAvatar };
