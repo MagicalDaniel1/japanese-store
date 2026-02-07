a8e259e33298c4d2da7dcccb7f554ab42221ab33
drajan <anastasia.fed2009@gmail.com>
2026-02-07 19:08:54 +0200

    Refactor updateInterests function formatting

--- japaneseStore/Features/User/UpdateIneterests.js
+++ japaneseStore/Features/User/UpdateIneterests.js
@@ -3,7 +3,10 @@ const axios = require("axios");
 const BASE = "http://localhost:3000/api/user";
 
 async function updateInterests(name, interests) {
-    const res = await axios.patch(`${BASE}/${name}/interests`, { interests });
+    const res = await axios.patch(
+        `${BASE}/${name}/interests`,
+        { interests }
+    );
     return res.data;
 }
 
